650b8b1d53194e09447480e9ac24e95cd156630c
ClaeroBluster <88840740+ClaeroBluster@users.noreply.github.com>
2022-01-11 19:25:02 +0530

    Add files via upload

---
+++ ass4/Ass4_2.java
@@ -0,0 +1,11 @@
+abstract class MyClass {
+    public void display() {
+       System.out.println("Method of abstract class");
+    }
+ }
+ class AbstractClass extends MyClass{
+    public static void main(String args[]) {
+     MyClass().display();
+     //new AbstractClass().display();
+    }
+ }
