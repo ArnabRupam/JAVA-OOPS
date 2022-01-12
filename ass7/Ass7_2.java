6a14274feba6d7b1b5438ee8fa4a3417611587db
ClaeroBluster <88840740+ClaeroBluster@users.noreply.github.com>
2022-01-12 09:43:23 +0530

    Add files via upload

---
+++ ass7/Ass7_2.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+class c {
+
+   public static void main(String args[])
+   {
+
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter a string");
+     String str=sc.nextLine();
+       char[] ch = new char[5];
+     System.out.println("Add some charecter");
+     for (int i = 0; i < 5; i++)
+     {
+           ch[i] =sc.next().charAt(0);
+       }
+       String string =new String(ch);
+        System.out.println(string);
+
+      String s3= str+string;
+       System.out.println(s3);
+       }
+ }
